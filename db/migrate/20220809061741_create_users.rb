d72ccb9dfe9c192b9acf08684acf96c826673e53
Christoph Moeller <christoph.moeller@disroot.org>
2022-08-09 15:29:56 +0900

    files created running rails db:migrate

--- db/migrate/20220809061741_create_users.rb
+++ db/migrate/20220809061741_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :users do |t|
+    create_table :users do |t|  # t stands for table
       t.string :name
       t.string :email
 
@@ -8,3 +8,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     end
   end
 end
+
+# The change method determines the change to be made to the database. 
+# change uses a Rails method called create_table to create a table in the database for storing users.
