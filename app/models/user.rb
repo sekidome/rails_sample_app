06bf015665469888dfe158c9d6e07b92008bd71d
Christoph Moeller <christoph.moeller@disroot.org>
2022-08-10 18:05:31 +0900

    adding test cases for and modifying user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 50 }
+    # could also be written like validates(:name, {presence: true...})
+    #VALID_EMAIL_REGEX is a constant, indicated in Ruby by a
+    #name starting with a capital letter
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "should be present" do 
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be presemt" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "mail should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email format is valid" do 
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    
+    valid_addresses.each do | valid_address|
+    @user.email = valid_address
+    assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+    foo@bar_baz.com foo@bar+baz.com]
+    
+    invalid_addresses.each do |inv_addr|
+        @user.email = inv_addr
+        assert_not @user.valid?, "#{inv_addr.inspect} should be invalid."
+    end
+  end
 end
